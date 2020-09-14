806676f584ebedb10ef6bb7acc62752e2735bd1c
Justin Pietsch <jpietsch@gmail.com>
2020-09-14 16:31:27 -0700

    adding catching bad verbs to device, using a uuid in the log, and filtering works for device show

--- suzieq/server/rest-server.py
+++ suzieq/server/rest-server.py
@@ -1,27 +1,32 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import logging
+import uuid
+
 
 from suzieq.sqobjects import *
 
-
+# TODO: logging to this file isn't working
+logging.FileHandler('/tmp/rest-server.log')
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-@app.get("/v1//table/show")
-async def read_table_show():
-    return await read_table('get')
-@app.get("/v1/table/{verb}")
+
+@app.get("/api/v1/table/{verb}")
 async def read_table(verb: str):
+    if verb == 'show':
+        verb = 'get'
     return getattr(tables.TablesObj(), verb)().to_json(orient="records")
 
-@app.get("/v1/device/show")
-async def read_device_show():
-    return await read_device('get')
-@app.get("/v1/device/{verb}")
+@app.get("/api/v1/device/{verb}")
 async def read_device(verb: str, hostname: str = "",
                       start_time: str = "", end_time: str = "",
                       view: str = "latest", namespace: str = ""):
-    return getattr(device.DeviceObj(hostname=hostname,
+    if verb == 'show':
+        verb = 'get'
+    try:
+        return getattr(device.DeviceObj(hostname=hostname,
                                     start_time=start_time,
                                     end_time=end_time,
                                     view=view,
@@ -29,3 +34,8 @@ async def read_device(verb: str, hostname: str = "",
                                     (hostname=hostname, 
                                     namespace=namespace)\
                                     .to_json(orient="records")
+        
+    except AttributeError as err:
+        u = uuid.uuid1()
+        logger.warning(f"invalid verb {verb}: {err} id={u}")
+        raise HTTPException(status_code=404, detail=f"{verb} not supported id={u}")

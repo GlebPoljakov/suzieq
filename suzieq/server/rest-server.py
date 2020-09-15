0cbe1c875bda5c8cf5deee41332d56dbd4258223
Justin Pietsch <jpietsch@gmail.com>
2020-09-14 17:06:48 -0700

    generic support for services and verbs. lots of things don't work, but more things work than before

--- suzieq/server/rest-server.py
+++ suzieq/server/rest-server.py
@@ -13,20 +13,36 @@ app = FastAPI()
 
 
 
-@app.get("/api/v1/table/{verb}")
-async def read_table(verb: str):
-    if verb == 'show':
-        verb = 'get'
-    return getattr(tables.TablesObj(), verb)().to_json(orient="records")
+# @app.get("/api/v1/table/{verb}")
+# async def read_table(verb: str):
+#     if verb == 'show':
+#         verb = 'get'
+#     try:
+#         return getattr(tables.TablesObj(), verb)().to_json(orient="records")
+#     # TODO: why can't i catch NotImplemented? that's what I want here?
+#     except Exception as err:
+#         u = uuid.uuid1()
+#         logger.warning(f"invalid verb {verb}: {err} id={u}")
+#         raise HTTPException(status_code=404, detail=f"{verb} not supported id={u}")
 
-@app.get("/api/v1/device/{verb}")
-async def read_device(verb: str, hostname: str = "",
+@app.get("/api/v1/{service}/{verb}")
+async def read_service(service: str, verb: str, hostname: str = "",
                       start_time: str = "", end_time: str = "",
                       view: str = "latest", namespace: str = ""):
     if verb == 'show':
         verb = 'get'
+
+    try: 
+        module = globals()[service]
+    except KeyError:
+        service = f"{service}s"
+        module = globals()[service]
+
+    svc = getattr(module, f"{service.title()}Obj")
+
     try:
-        return getattr(device.DeviceObj(hostname=hostname,
+
+        return getattr(svc(hostname=hostname,
                                     start_time=start_time,
                                     end_time=end_time,
                                     view=view,
@@ -34,8 +50,15 @@ async def read_device(verb: str, hostname: str = "",
                                     (hostname=hostname, 
                                     namespace=namespace)\
                                     .to_json(orient="records")
-        
+    
+    # TODO: why can't i catch NotImplemented? that's what I want here?  
     except AttributeError as err:
         u = uuid.uuid1()
-        logger.warning(f"invalid verb {verb}: {err} id={u}")
-        raise HTTPException(status_code=404, detail=f"{verb} not supported id={u}")
+        logger.warning(f"{verb} not supported for {service}: {err} id={u}")
+        raise HTTPException(status_code=404, 
+                            detail=f"{verb} not supported for {service}: {err} id={u}")
+    except Exception as err:
+        u = uuid.uuid1()
+        logger.warning(f"exceptional exception {verb} for {service}: {err} id={u}")
+        raise HTTPException(status_code=404, 
+                            detail=f"exceptional exception {verb} for {service}: {err} id={u}")
